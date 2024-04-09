4e0d9218720c1ec6e1a46f72fae3c6a1b8e70da9
Ян <peroff007@gmail.com>
2024-04-09 20:47:03 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,28 @@ public class Main {
                     System.out.println(year + " год — невисокосный год");
                 }
             }
+                //Task 2
+            public static void recommendAppVersion(int clientOS, int deviceYear) {
+                System.out.print("Установите ");
+                if (deviceYear <= 2015) {
+                    System.out.print("облегченную ");
+                }
+                System.out.print("версию приложения для ");
+                if (clientOS == 0) {
+                    System.out.println("iOS");
+                } else {
+                    System.out.println("Android");
+                }
+            }
             public static void main(String[] args) {
                 //Task 1
-            int year = 2019;
+            int year = 2010;
             printIsLeapYear(year);
+            System.out.println(" ");
+                //Task 2
+            int clientOS = 0;
+            recommendAppVersion(clientOS,year);
+
             }
 
         }
