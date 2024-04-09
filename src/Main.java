b6870eb850204ce53871d072fbee5c03139ff5af
Ян <peroff007@gmail.com>
2024-04-09 20:26:05 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 import java.time.LocalDate;
 public class Main {
                 //Task 1
-            public static void isLeapYear(int year) {
-                boolean yearIsLeap = isLeap(year);
-                printIsLeapYearResult(year, yearIsLeap);
-            }
-            public static boolean isLeap(int year) {
-                return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-            }
-            public static void printIsLeapYearResult(int year, boolean yearIsLeap) {
-                if (yearIsLeap) {
+            public static void printIsLeapYear(int year) {
+                if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
                     System.out.println(year + " год — високосный год");
                 } else {
                     System.out.println(year + " год — невисокосный год");
@@ -17,8 +10,8 @@ public class Main {
             }
             public static void main(String[] args) {
                 //Task 1
-            int year = 2016;
-            isLeapYear(year);
+            int year = 2019;
+            printIsLeapYear(year);
             }
 
         }
